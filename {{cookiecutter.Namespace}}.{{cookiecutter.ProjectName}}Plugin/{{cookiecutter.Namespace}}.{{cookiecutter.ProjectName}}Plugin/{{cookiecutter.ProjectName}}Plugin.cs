4593180460d2c5ccab70c72facf21b0d636c6e58
anu-vadivel <119852287+anu-vadivel@users.noreply.github.com>
2022-12-23 15:59:06 +0530

    Initial commit

---
+++ {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}Plugin/{{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}Plugin/{{cookiecutter.ProjectName}}Plugin.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}Plugin;
+
+public class {{cookiecutter.ProjectName}}Plugin : {{cookiecutter.PluginInterface}}
+{
+   private readonly ILogger<{{cookiecutter.ProjectName}}Plugin> _logger;
+
+   public {{cookiecutter.ProjectName}}Plugin(ILogger<{{cookiecutter.ProjectName}}Plugin> logger)
+   {
+      _logger = logger;
+   }
+
+   public void MakePayment()
+   {
+      _logger.LogInformation("I am in payment");
+   }
+}
